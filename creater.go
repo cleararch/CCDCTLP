e04eceb7750a0f49cdabd400ead0f2a90c237864
KirisameMa <jackeyz2022@mail.ru>
2022-06-25 17:19:34 +0800

    deb extrat finished

--- creater.go
+++ creater.go
@@ -14,10 +14,36 @@ func unpack_deb(name string, dir string) bool {
 		return false
 	}
 }
+func sys_bind(dir string) {
+	//挂载/proc /tmp /run /sys
+	if mount_bind("/proc", dir+"/proc") == false {
+		return false
+	}
+	if mount_bind("/tmp", dir+"/tmp") == false {
+		return false
+	}
+	if mount_bind("/run", dir+"/run") == false {
+		return false
+	}
+	if mount_bind("/run", dir+"/sys") == false {
+		return false
+	}
+	return true
+}
 func deb_create_package(dir string, package_deb string) bool {
 	err := os.MkdirAll(dir)
 	if err != nil {
 		return false
 	}
 	os.Chdir(dir)
+	if unpack_deb(package_deb, dir) == false {
+		return false
+	}
+	os.Mkdir(dir + "/root_sys")
+	if mount_bind("/", dir+"/root_sys") == false {
+		return false
+	}
+	if sys_bind() == false {
+		return false
+	}
 }
