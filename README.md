2e6366ab2d59138b8dc3f56d167ebe61557bf0e4
jack <jackeyz2022@mail.ru>
2022-09-16 23:17:47 +0800

    重新来过

--- README.md
+++
@@ -1,4 +0,0 @@
-# CCDCTLP
-Cleararch's Cross-Distribution Compatibility Toolkit for Linux Packages 
-
-Cleararch的Linux软件包跨发行版兼容工具包
--- ccdctlp.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println(Deb_uncompress("/media/jack/data/tmp", "/home/jack/下载/hello.deb"))
-}
